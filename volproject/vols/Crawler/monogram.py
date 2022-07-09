d3c1e4c6f45ef997a468c9b512f33466e80078e3
You Zhe <money110209@gmail.com>
2022-07-09 23:03:49 +0800

    fix condition of HTTP error

--- volproject/vols/Crawler/monogram.py
+++ volproject/vols/Crawler/monogram.py
@@ -7,9 +7,16 @@ def find_monogram(word):
     request =  Request(url,headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
     })
-    Web = urlopen(request)
+    try:
+        Web = urlopen(request)
+    except:
+        return "not found"
+
+    # 找不到就回傳 not found
     Data = Web.read().decode("utf-8")
     bs4_Data = BeautifulSoup(Data,'html.parser')
+    if(bs4_Data.find(name="span",id_="ch_lblCustomMessage" )):
+         return "not find"
 
     find_Data =  bs4_Data.find(name="div",class_="result-block-api").find(name="div",class_="blue-box-wrap").find_all(name="i",class_="verbtxt")
     third_verb = find_Data[2].text
@@ -18,4 +25,4 @@ def find_monogram(word):
 
 if __name__ == "__main__":
     word = input("input word ")
-    find_monogram(word)
+    print(find_monogram(word))
